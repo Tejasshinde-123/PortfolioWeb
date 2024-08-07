98e0f12a815432115e81ba94ce8cbd1889761bd5
Tejasshinde-123 <tejasrshinde2000@gmail.com>
2024-08-07 10:34:03 +0530

    second commit

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import './Profile.css'
+import React from 'react';
+import './Profile.css';
+import resume from '../../assets/Resume.pdf'
+import profilePhoto from '../../assets/ProfilePhoto.jpeg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-import profilePhoto from '../../assets/ProfilePhoto.jpeg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Profile = () => {
   return (
     <div id='home' className='profile'>
-     <img className='profilephoto' src={profilePhoto} alt="" />
+      <img className='profilephoto' src={profilePhoto} alt="Profile" />
       <h1><span>I am Tejas Shinde, <br /> </span> Web Developer</h1>
       <div className="profileaction">
-        <div className="connectwithprofile"><AnchorLink className='anchor-link' offset={50} href='#contact'>Contact Me</AnchorLink></div>
-        <div className="profileresume">My Resume</div>
+        <div className="connectwithprofile">
+          <AnchorLink className='anchor-link' offset={50} href='#contact'>Contact Me</AnchorLink>
+        </div>
+        <div className="profileresume">
+        
+            <a href={resume} download="Resume.pdf">My Resume</a>
+         
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
